2dde7dc4da4a91cdb52d3431cdf8a228ab6f83e8
tecnocel <ventas@tecnocel.com.ar>
2018-11-21 12:34:11 -0300

    agendas

--- scripts/catalog.js
+++ scripts/catalog.js
@@ -3,26 +3,31 @@
 
 let articulos = document.getElementsByClassName("rubro");
 const catalog = document.getElementById("catalog");
+const rubros = document.getElementById("rubros");
 const volver = document.getElementById("volver");
 
 const catalog_content = document.getElementById("catalog").innerHTML;
+
+
 
 function addListener(){
 
     for (let i = 0; i < articulos.length; i++){
         
         articulos[i].onclick = function(e){seleccion(e.target.id)};
+        
 
     }
 
 }
 
 volver.onclick = function(){
-    catalog.className = "show";
+
+
 }
 
 function seleccion(id){
-    catalog.className = "hide";
+
 }
 
 // Pone a la escucha cada rubro.
